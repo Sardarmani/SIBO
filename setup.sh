6928c03b4cb0176016c5c6b52d7064cf9ac1d5ac
Muneeb <57819194+Sardarmani@users.noreply.github.com>
2024-10-17 12:54:08 +0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,11 @@
 mkdir -p ~/.streamlit/
 
 echo "\
+[general]\n\
+email = \"muhammadmuneebtariq07.com\"\n\
+" > ~/.streamlit/credentials.toml
+
+echo "\
 [server]\n\
 headless = true\n\
 enableCORS=false\n\
